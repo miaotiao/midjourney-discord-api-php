87fc223e219891bc0859453763d897d216636f10
miaotiao <1397701721@qq.com>
2023-09-07 16:27:26 +0800

    add proxy && code format

--- src/Midjourney.php
+++ src/Midjourney.php
@@ -4,8 +4,10 @@ namespace Ferranfg\MidjourneyPhp;
 
 use Exception;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 
-class Midjourney {
+class Midjourney
+{
 
     private const API_URL = 'https://discord.com/api/v9';
 
@@ -27,38 +29,44 @@ class Midjourney {
 
     private static $user_id;
 
-    public function __construct($channel_id, $oauth_token)
+    /**
+     *
+     * @param $channel_id
+     * @param $oauth_token
+     * @param string|null $proxy
+     * @throws GuzzleException
+     */
+    public function __construct($channel_id, $oauth_token, string $proxy = null)
     {
+
         self::$channel_id = $channel_id;
         self::$oauth_token = $oauth_token;
 
         self::$client = new Client([
-            'base_uri' => self::API_URL,
+            'base_uri' => $proxy ?? self::API_URL,
             'headers' => [
                 'Authorization' => self::$oauth_token
             ]
         ]);
 
         $request = self::$client->get('channels/' . self::$channel_id);
-        $response = json_decode((string) $request->getBody());
+        $response = json_decode((string)$request->getBody());
 
         self::$guild_id = $response->guild_id;
 
         $request = self::$client->get('users/@me');
-        $response = json_decode((string) $request->getBody());
+        $response = json_decode((string)$request->getBody());
 
         self::$user_id = $response->id;
     }
 
     private static function firstWhere($array, $key, $value = null)
     {
-        foreach ($array as $item)
-        {
+        foreach ($array as $item) {
             if (
                 (is_callable($key) and $key($item)) or
                 (is_string($key) and str_starts_with($item->{$key}, $value))
-            )
-            {
+            ) {
                 return $item;
             }
         }
@@ -113,8 +121,7 @@ class Midjourney {
 
         $imagine_message = null;
 
-        while (is_null($imagine_message))
-        {
+        while (is_null($imagine_message)) {
             $imagine_message = $this->getImagine($prompt);
 
             if (is_null($imagine_message)) sleep(8);
@@ -126,20 +133,19 @@ class Midjourney {
     public function getImagine(string $prompt)
     {
         $response = self::$client->get('channels/' . self::$channel_id . '/messages');
-        $response = json_decode((string) $response->getBody());
+        $response = json_decode((string)$response->getBody());
 
-        $raw_message = self::firstWhere($response, function ($item) use ($prompt)
-        {
+        $raw_message = self::firstWhere($response, function ($item) use ($prompt) {
             return (
                 str_starts_with($item->content, "**{$prompt}** - <@" . self::$user_id . '>') and
-                ! str_contains($item->content, '%') and
+                !str_contains($item->content, '%') and
                 str_ends_with($item->content, '(fast)')
             );
         });
 
         if (is_null($raw_message)) return null;
 
-        return (object) [
+        return (object)[
             'id' => $raw_message->id,
             'prompt' => $prompt,
             'raw_message' => $raw_message
@@ -148,21 +154,18 @@ class Midjourney {
 
     public function upscale($message, int $upscale_index = 0)
     {
-        if ( ! property_exists($message, 'raw_message'))
-        {
+        if (!property_exists($message, 'raw_message')) {
             throw new Exception('Upscale requires a message object obtained from the imagine/getImagine methods.');
         }
 
-        if ($upscale_index < 0 or $upscale_index > 3)
-        {
+        if ($upscale_index < 0 or $upscale_index > 3) {
             throw new Exception('Upscale index must be between 0 and 3.');
         }
 
         $upscale_hash = null;
         $raw_message = $message->raw_message;
 
-        if (property_exists($raw_message, 'components') and is_array($raw_message->components))
-        {
+        if (property_exists($raw_message, 'components') and is_array($raw_message->components)) {
             $upscales = $raw_message->components[0]->components;
 
             $upscale_hash = $upscales[$upscale_index]->custom_id;
@@ -188,8 +191,7 @@ class Midjourney {
 
         $upscaled_photo_url = null;
 
-        while (is_null($upscaled_photo_url))
-        {
+        while (is_null($upscaled_photo_url)) {
             $upscaled_photo_url = $this->getUpscale($message, $upscale_index);
 
             if (is_null($upscaled_photo_url)) sleep(3);
@@ -200,33 +202,29 @@ class Midjourney {
 
     public function getUpscale($message, $upscale_index = 0)
     {
-        if ( ! property_exists($message, 'raw_message'))
-        {
+        if (!property_exists($message, 'raw_message')) {
             throw new Exception('Upscale requires a message object obtained from the imagine/getImagine methods.');
         }
 
-        if ($upscale_index < 0 or $upscale_index > 3)
-        {
+        if ($upscale_index < 0 or $upscale_index > 3) {
             throw new Exception('Upscale index must be between 0 and 3.');
         }
 
         $prompt = $message->prompt;
 
         $response = self::$client->get('channels/' . self::$channel_id . '/messages');
-        $response = json_decode((string) $response->getBody());
+        $response = json_decode((string)$response->getBody());
 
         $message_index = $upscale_index + 1;
         $message = self::firstWhere($response, 'content', "**{$prompt}** - Image #{$message_index} <@" . self::$user_id . '>');
 
-        if (is_null($message))
-        {
+        if (is_null($message)) {
             $message = self::firstWhere($response, 'content', "**{$prompt}** - Upscaled by <@" . self::$user_id . '> (fast)');
         }
 
         if (is_null($message)) return null;
 
-        if (property_exists($message, 'attachments') and is_array($message->attachments))
-        {
+        if (property_exists($message, 'attachments') and is_array($message->attachments)) {
             $attachment = $message->attachments[0];
 
             return $attachment->url;
@@ -241,7 +239,7 @@ class Midjourney {
 
         $upscaled_photo_url = $this->upscale($imagine, $upscale_index);
 
-        return (object) [
+        return (object)[
             'imagine_message_id' => $imagine->id,
             'upscaled_photo_url' => $upscaled_photo_url
         ];
